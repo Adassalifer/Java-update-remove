7154a88500962f64a37afbc0c716a67f04495206
Ada Jeanneffer <adassamoda@gmail.com>
2023-07-31 22:53:16 -0300

    create

---
+++ curso_java/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	double x = 100;	
+	double y = 100;
+	
+	while (x != y) {
+	System.out.println("olha");
+	x = Math.sqrt(y);
+	
+		
+		
+	}
+	
+		
+
+
+
+
+}
+}
